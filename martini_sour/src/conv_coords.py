618a228c4d76bbb17b7989d44a30d726d774dcc1
Selim Sami <sesami141@gmail.com>
2021-09-23 14:44:51 +0200

    conv_coords module rewrite initial commit

--- martini_sour/src/conv_coords.py
+++ martini_sour/src/conv_coords.py
@@ -11,102 +11,184 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import random
+
 import numpy as np
 import MDAnalysis as mda
-from numpy.linalg import norm
+from itertools import chain
+from scipy.spatial.transform import Rotation
+import random
 
-def u_vect(vect):
-    return(vect/norm(vect))
 
-def norm_sphere():
-    v_sphere = np.random.normal(0.0, 1, (5000,3))
-    return(np.array([ u_vect(vect) for vect in v_sphere]))
+bead_type_dict = {'water': ['DN', 'H+'],
+                  'acid': ['D', 'DP', 'H+'],
+                  'base': ['D', 'DP'],
+                  'acid-deprot': ['D', 'DP'],
+                  'base-prot': ['D', 'DP', 'H+']}
 
-def count_prot_res(atoms,resname):
-    waters = 0
-    for atom in atoms:
-        if atom.resname == resname:
-           waters = waters + 1
-    return(waters)
+
+def get_random_vectors(n_randoms=1000, length=1.7):
+    """
+    Generate randomly rotated 3 points that all have 120 degree angle to the
+    center point with a given vector length. This is done n_randoms times.
+    0.8660254 ~= 0.75**0.5
+
+    Parameters
+    ----------
+    length : float, optional
+        Length of the vectors. The default is 1.7 Angstrom.
+
+    Returns
+    -------
+    (n_randoms, 3, 3) Numpy array
+
+    """
+
+    vectors = [[1, 0, 0], [-0.5, 0.8660254, 0], [-0.5, -0.8660254, 0]]
+    random_rots = Rotation.random(n_randoms)
+
+    random_vecs = []
+    for vec in vectors:
+        random_vecs.append(random_rots.apply(vec))
+    random_vecs = np.transpose(random_vecs, (1, 0, 2))
+
+    return random_vecs*length
+
+
+class Particles():
+    def __init__(self):
+        self.resids = []
+        self.resnames = []
+        self.names = []
+        self.indices = []
+        self.coords = []
+
+    def add_particle(self, resid, resname, name, coord):
+        self.resids.append(resid)
+        self.resnames.append(resname)
+        self.names.append(name)
+        self.coords.append(coord)
+
+    def set_indices(self, start=1):
+        self.indices = np.arange(start, start+len(self)) % 100000
+
+    def set_resids(self, start=1):
+        self.resids = np.arange(start, start+len(self.resids)) % 100000
+
+    def convert_coords_to_nm(self):
+        self.coords = np.array(self.coords)*0.1
+
+    def __iter__(self):
+        return zip(self.resids, self.resnames, self.names, self.indices, self.coords)
+
+    def __len__(self):
+        return len(self.resids)
+
+
+def check_input(args):
+    if len(args.bead_type) != len(args.sel):
+        raise ValueError('Unmatching number of -bead and -sel was provided.')
+
+
+def do_selections(uni, selection_commands):
+    """
+    Currently, if an atom is in multiple selections, the last one selection
+    is taken.
+
+    Parameters
+    ----------
+    uni : MDAnalysis Universe object
+    selection_commands : list of strings
+        List of MDAnalysis selection commands
+
+    Returns
+    -------
+    selections : 1D Numpy array
+        Array with the selection index of each atom. -1 if not in any selection
+
+    """
+
+    selections = np.full(len(uni.atoms), -1)
+
+    for i, selection_command in enumerate(selection_commands):
+        idx = uni.select_atoms(selection_command).indices
+        selections[idx] = i
+
+    return selections
+
+
+def add_extra_particles(beads, protons, atom, particle_list, random_vecs):
+
+    positions = random_vecs[random.randint(0, 999)]
+
+    for i, particle in enumerate(particle_list):
+        if particle == 'H+':
+            protons.add_particle(None, particle, 'POS', atom.position + positions[i])
+        else:
+            beads.add_particle(atom.resid, atom.resname, particle, atom.position + positions[i])
+
+    return beads, protons
+
+
+def do_one_atom(selection, bead_types, random_vectors, beads, protons, atom):
+    """
+    For a single atom, add the original bead, then if it is a titratable bead,
+    add necessary extra particles
+    """
+
+    beads.add_particle(atom.resid, atom.resname, atom.name, atom.position)
+
+    if selection >= 0:
+        particle_list = bead_type_dict[bead_types[selection]]
+        beads, protons = add_extra_particles(beads, protons, atom, particle_list, random_vectors)
+
+    return beads, protons
+
+
+def prepare_for_writing(beads, protons, dimensions):
+    """
+    Do necessary unit (coordinates) and number (resids, indices) conversions
+    before writing.
+    """
+
+    beads.set_indices()
+    beads.resids = np.array(beads.resids) % 100000
+    protons.set_indices(start=beads.indices[-1]+1)
+    protons.set_resids(start=beads.resids[-1]+1)
+
+    beads.convert_coords_to_nm()
+    protons.convert_coords_to_nm()
+    dimensions *= 0.1
+
+    return beads, protons, dimensions
+
+
+def write_ff(out_file, dimensions, beads, protons):
+    with open(out_file, 'w') as file:
+        file.write('titratable MARTINI \n')
+        file.write(f'{len(beads)+len(protons):<12d}\n')
+        for resid, resname, name, idx, coord in chain(beads, protons):
+            file.write(f'{resid:>5d}{resname:<5s}{name:>5s}{idx:5d}'
+                       f'{coord[0]:8.3F}{coord[1]:8.3F}{coord[2]:8.3F}\n')
+        file.write(f'    {dimensions[0]:>8.3F}{dimensions[1]:>8.3F}{dimensions[2]:>8.3F}\n')
+
+
+def initialize(args):
+    check_input(args)
+    beads = Particles()
+    protons = Particles()
+    random_vecs = get_random_vectors()
+
+    return beads, protons, random_vecs
+
 
 def conv_coords(args):
-    u = mda.Universe(args.input_file)
+    beads, protons, random_vecs = initialize(args)
 
-    # 1. Count how many beads need to be converted 
-    #pH_beads = count_prot_res(u.atoms, args.resname)
-    pH_beads = len(u.select_atoms(args.sel))
+    uni = mda.Universe(args.input_file)
+    selections = do_selections(uni, args.sel)
 
-    # 2. Do some bookeping
-    particles={"acid":3,"water":2,"base":2,"acid-deprot":2,"base-prot":3}
-    total_beads = len(u.atoms) + pH_beads * particles[args.bead_type]
+    for atom, selection in zip(uni.atoms, selections):
+        beads, protons = do_one_atom(selection, args.bead_type, random_vecs, beads, protons, atom)
 
-    # 3. Define the formats
-    atom_format='{:>5d}{:<5s}{:>5s}{:5d}{:8.3F}{:8.3F}{:8.3F}{}'
-
-
-    # 4. Write all beads but the protons
-    selection = u.select_atoms(args.sel)
-
-    with open(args.out_file,'w') as out_file:
-
-         protons = []
-         atom_count    = 0
-         vectors = norm_sphere()
-
-         out_file.write('titratable MARTINI \n')
-         out_file.write('{:>12d}{}'.format(total_beads,'\n'))
-         for atom in u.atoms:
-             if atom in selection:
-                vector = vectors[random.randint(0, len(vectors) - 1)]
-                old_pos = 0.1*atom.position
-                prot = old_pos + vector * 0.17
-                dum  = old_pos - vector * 0.17
-                atom_count+=1
-                res=atom.resname
-                out_file.write(atom_format.format(atom.resindex,res,atom.name,atom_count,old_pos[0],old_pos[1],old_pos[2],'\n'))
-                atom_count+=1
-                out_file.write(atom_format.format(atom.resindex,res,'DN',atom_count,dum[0],dum[1],dum[2],'\n'))
-
-                if args.bead_type == 'water':
-                   protons.append(prot)
-                elif args.bead_type == 'acid':
-                     v = vector * 0.17
-                     v = np.array([v[1],v[0],v[2]])
-                     prot_new = old_pos + v
-                     protons.append(prot_new)
-                     atom_count+=1
-                     res=atom.resname
-                     out_file.write(atom_format.format(atom.resindex,res,'DP',atom_count,prot[0],prot[1],prot[2],'\n'))
-                elif args.bead_type == 'acid-deprot':
-                     atom_count+=1
-                     res=atom.resname
-                     out_file.write(atom_format.format(atom.resindex,res,'DP',atom_count,prot[0],prot[1],prot[2],'\n'))
-                elif args.bead_type == "base" :
-                      atom_count+=1
-                      res=atom.resname
-                      out_file.write(atom_format.format(atom.resindex,res,'DP',atom_count,prot[0],prot[1],prot[2],'\n'))
-                elif args.bead_type == "base-prot":
-                      atom_count += 1
-                      res=atom.resname
-                      out_file.write(atom_format.format(atom.resindex,res,'DP',atom_count,prot[0],prot[1],prot[2],'\n'))
-                      exc_prot = np.array([-prot[1],prot[0],prot[2]])
-                      protons.append(prot)
-             else:
-
-                pos = 0.1*atom.position
-                atom_count+=1
-                res=atom.resname
-                out_file.write(atom_format.format(atom.resindex,res,atom.name,atom_count,pos[0],pos[1],pos[2],'\n'))
-    # 5. Write all protons to file
-         resid_count = atom.resindex
-         print(atom_count)
-
-         for pos in protons:
-#         print('go_here')
-             resid_count = resid_count + 1
-             atom_count+=1
-             out_file.write(atom_format.format(resid_count,'H+','POS',atom_count,pos[0],pos[1],pos[2],'\n'))
-
-    # 6. Write box coodinates
-    out_file.write('    {:>8.3F}{:>8.3F}{:>8.3F}{}'.format(0.1*u.trajectory[-1].dimensions[0],0.1*u.trajectory[-1].dimensions[1],0.1*u.trajectory[-1].dimensions[2],'\n'))
+    beads, protons, dimensions = prepare_for_writing(beads, protons, uni.dimensions)
+    write_ff(args.out_file, dimensions, beads, protons)
