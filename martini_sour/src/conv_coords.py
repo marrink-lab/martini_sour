ad40bf074184fa40a5d965a69985f4aff4ede80c
Selim Sami <sesami141@gmail.com>
2021-10-14 15:51:06 +0200

    fixes to conv_coords

--- martini_sour/src/conv_coords.py
+++ martini_sour/src/conv_coords.py
@@ -14,16 +14,15 @@
 
 import numpy as np
 import MDAnalysis as mda
-from itertools import chain
 from scipy.spatial.transform import Rotation
 import random
 
 
-bead_type_dict = {'water': ['DN', 'H+'],
-                  'acid': ['D', 'DP', 'H+'],
-                  'base': ['D', 'DP'],
-                  'acid-deprot': ['D', 'DP'],
-                  'base-prot': ['D', 'DP', 'H+']}
+BEAD_TYPES = {'water': ['DN', 'H+'],
+              'acid': ['D', 'DP', 'H+'],
+              'base': ['D', 'DP'],
+              'acid-deprot': ['D', 'DP'],
+              'base-prot': ['D', 'DP', 'H+']}
 
 
 def get_random_vectors(n_randoms=1000, length=1.7):
@@ -46,10 +45,9 @@ def get_random_vectors(n_randoms=1000, length=1.7):
     vectors = [[1, 0, 0], [-0.5, 0.8660254, 0], [-0.5, -0.8660254, 0]]
     random_rots = Rotation.random(n_randoms)
 
-    random_vecs = []
-    for vec in vectors:
-        random_vecs.append(random_rots.apply(vec))
-    random_vecs = np.transpose(random_vecs, (1, 0, 2))
+    random_vecs = np.empty((n_randoms, 3, 3))
+    for i, vec in enumerate(vectors):
+        random_vecs[:, i, :] = random_rots.apply(vec)
 
     return random_vecs*length
 
@@ -68,18 +66,6 @@ class Particles():
         self.names.append(name)
         self.coords.append(coord)
 
-    def set_indices(self, start=1):
-        self.indices = np.arange(start, start+len(self)) % 100000
-
-    def set_resids(self, start=1):
-        self.resids = np.arange(start, start+len(self.resids)) % 100000
-
-    def convert_coords_to_nm(self):
-        self.coords = np.array(self.coords)*0.1
-
-    def __iter__(self):
-        return zip(self.resids, self.resnames, self.names, self.indices, self.coords)
-
     def __len__(self):
         return len(self.resids)
 
@@ -89,11 +75,8 @@ def check_input(args):
         raise ValueError('Unmatching number of -bead and -sel was provided.')
 
 
-def do_selections(uni, selection_commands):
+def select_titratable_beads(uni, selection_commands):
     """
-    Currently, if an atom is in multiple selections, the last one selection
-    is taken.
-
     Parameters
     ----------
     uni : MDAnalysis Universe object
@@ -111,84 +94,100 @@ def do_selections(uni, selection_commands):
 
     for i, selection_command in enumerate(selection_commands):
         idx = uni.select_atoms(selection_command).indices
+
+        if len(idx) == 0:
+            raise ValueError(f'Selection command "{selection_command}" contains zero particles. '
+                             'Check your input.')
+        if np.any(selections[idx] != -1):
+            raise ValueError(f'Selection command "{selection_command}" contains particles from '
+                             'a different selection. Check your input.')
+
         selections[idx] = i
 
     return selections
 
 
-def add_extra_particles(beads, protons, atom, particle_list, random_vecs):
+def add_extra_particles(beads, protons, atom, particle_list, random_vecs, prot_name):
+    """
+    Select a random vector and add each necessary particle to either the protons or
+    beads object.
 
-    positions = random_vecs[random.randint(0, 999)]
+    Parameters
+    ----------
+    beads : Particles object
+
+    protons : Particles object
+
+    atom : MDAnalysis atom object
+
+    particle_list : list
+        all the necessary particles for the corresponding BEAD_TYPE
+    random_vecs : (n_randoms, 3, 3) Numpy array
+
+    """
+
+    vectors = random_vecs[random.randint(0, 999)]
 
     for i, particle in enumerate(particle_list):
         if particle == 'H+':
-            protons.add_particle(None, particle, 'POS', atom.position + positions[i])
+            protons.add_particle(None, particle, prot_name, atom.position + vectors[i])
         else:
-            beads.add_particle(atom.resid, atom.resname, particle, atom.position + positions[i])
+            beads.add_particle(atom.resid, atom.resname, particle, atom.position + vectors[i])
 
     return beads, protons
 
 
-def do_one_atom(selection, bead_types, random_vectors, beads, protons, atom):
+def create_titratable_universe(beads, protons, dimensions):
     """
-    For a single atom, add the original bead, then if it is a titratable bead,
-    add necessary extra particles
-    """
+    Combines all beads and protons to create the titratable MDAnalysis universe
 
-    beads.add_particle(atom.resid, atom.resname, atom.name, atom.position)
+    Parameters
+    ----------
+    beads : Particles object
 
-    if selection >= 0:
-        particle_list = bead_type_dict[bead_types[selection]]
-        beads, protons = add_extra_particles(beads, protons, atom, particle_list, random_vectors)
+    protons : Particles object
 
-    return beads, protons
+    dimensions : (3) Numpy array
+        Dimensions of the unit cell (Angstrom)
 
-
-def prepare_for_writing(beads, protons, dimensions):
-    """
-    Do necessary unit (coordinates) and number (resids, indices) conversions
-    before writing.
     """
 
-    beads.set_indices()
-    beads.resids = np.array(beads.resids) % 100000
-    protons.set_indices(start=beads.indices[-1]+1)
-    protons.set_resids(start=beads.resids[-1]+1)
+    n_atoms = len(beads)+len(protons)
+    max_bead_resid = max(beads.resids)
+    resids = np.append(beads.resids, 1 + np.arange(max_bead_resid, max_bead_resid+len(protons)))
+    resnames = beads.resnames + protons.resnames
+    names = beads.names + protons.names
 
-    beads.convert_coords_to_nm()
-    protons.convert_coords_to_nm()
-    dimensions *= 0.1
+    uni = mda.Universe.empty(n_atoms, trajectory=True, n_residues=n_atoms,
+                             atom_resindex=np.arange(0, n_atoms),
+                             residue_segindex=np.zeros(n_atoms))
 
-    return beads, protons, dimensions
+    uni.add_TopologyAttr('resid', resids)
+    uni.add_TopologyAttr('resname', resnames)
+    uni.add_TopologyAttr('name', names)
 
+    uni.atoms.positions = np.append(beads.coords, protons.coords, axis=0)
+    uni.dimensions = dimensions
 
-def write_ff(out_file, dimensions, beads, protons):
-    with open(out_file, 'w') as file:
-        file.write('titratable MARTINI \n')
-        file.write(f'{len(beads)+len(protons):<12d}\n')
-        for resid, resname, name, idx, coord in chain(beads, protons):
-            file.write(f'{resid:>5d}{resname:<5s}{name:>5s}{idx:5d}'
-                       f'{coord[0]:8.3F}{coord[1]:8.3F}{coord[2]:8.3F}\n')
-        file.write(f'    {dimensions[0]:>8.3F}{dimensions[1]:>8.3F}{dimensions[2]:>8.3F}\n')
+    return uni
 
 
-def initialize(args):
+def conv_coords(args):
     check_input(args)
+
     beads = Particles()
     protons = Particles()
     random_vecs = get_random_vectors()
 
-    return beads, protons, random_vecs
-
-
-def conv_coords(args):
-    beads, protons, random_vecs = initialize(args)
-
     uni = mda.Universe(args.input_file)
-    selections = do_selections(uni, args.sel)
+    selections = select_titratable_beads(uni, args.sel)
 
     for atom, selection in zip(uni.atoms, selections):
-        beads, protons = do_one_atom(selection, args.bead_type, random_vecs, beads, protons, atom)
+        beads.add_particle(atom.resid, atom.resname, atom.name, atom.position)
+        if selection >= 0:
+            particle_list = BEAD_TYPES[args.bead_type[selection]]
+            beads, protons = add_extra_particles(beads, protons, atom, particle_list, random_vecs,
+                                                 args.prot)
 
-    beads, protons, dimensions = prepare_for_writing(beads, protons, uni.dimensions)
-    write_ff(args.out_file, dimensions, beads, protons)
+    titr_uni = create_titratable_universe(beads, protons, uni.dimensions)
+    titr_uni.atoms.write(args.out_file)
